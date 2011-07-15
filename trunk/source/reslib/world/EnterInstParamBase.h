382837006cea788307c25410c3330667e002b5ec
lujf1978@gmail.com <lujf1978@gmail.com@e4d6a914-db52-454f-4d7e-83b15fb67026>
2011-07-15 08:34:54 +0000

    进入副本控制实现，支持c++实现，条件判断，lua脚本

---
+++ trunk/source/reslib/world/EnterInstParamBase.h
@@ -0,0 +1,16 @@
+/**
+* reslib
+*
+* @category		∏±±æ≈‰÷√
+* @package		
+* @author		Created by Lu Jianfeng ( lujf1978@163.com ), 2011
+*/
+#ifndef __ENTERINSTPARAMBASE__H__
+#define __ENTERINSTPARAMBASE__H__
+
+class EnterInstParamBase
+{
+
+};
+
+#endif	//__ENTERINSTPARAMBASE__H__
---
+++ trunk/source/reslib/world/EnterInstParamOfTask.h
@@ -0,0 +1,18 @@
+/**
+* reslib
+*
+* @category		∏±±æ≈‰÷√
+* @package		
+* @author		Created by Lu Jianfeng ( lujf1978@163.com ), 2011
+*/
+#ifndef __ENTERINSTPARAMOFTASK__H__
+#define __ENTERINSTPARAMOFTASK__H__
+
+#include "reslib/world/EnterInstParamBase.h"
+
+class EnterInstParamOfTask : public EnterInstParamBase
+{
+
+};
+
+#endif	//__ENTERINSTPARAMOFTASK__H__
